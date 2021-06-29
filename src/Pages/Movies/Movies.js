e8a4557e9bbc9e5db2b72a5e25c57168112ec6a4
Daniel Um <39230120+umdaniel@users.noreply.github.com>
2021-06-29 10:02:00 -0400

    Movie, TV and Search Page

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -3,16 +3,21 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import SingleContent from '../../components/SingleContent/SingleContent';
 import CustomPagination from '../../components/Pagination/CustomPagination';
+import Genres from "../../components/Genres";
+import useGenre from "../../hooks/useGenre";
 
 const Movies = () => {
 
     const [page, setPage] = useState(1);
     const [content, setContent] = useState([]);
     const [numOfPages, setNumOfPages] = useState();
+    const [selectedGenres, setSelectedGenres] = useState([]);
+    const [genres, setGenres] = useState([]);
+    const genreforURL = useGenre(selectedGenres);
 
     const fetchMovies = async () => {
         const { data } = await axios.get(
-            `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+            `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
         );
 
         setContent(data.results);
@@ -21,11 +26,21 @@ const Movies = () => {
 
     useEffect(() => {
         fetchMovies();
-    }, []);
+    }, [page, genreforURL]);
 
     return(
         <div>
             <span className="pageTitle">Movies</span>
+
+            <Genres
+            type="movie"
+            selectedGenres={selectedGenres}
+            setSelectedGenres={setSelectedGenres}
+            genres={genres}
+            setGenres={setGenres}
+            setPage={setPage}
+            />
+
             <div className="trending">
                 {content && content.map((c) => (
                     <SingleContent 
@@ -38,7 +53,9 @@ const Movies = () => {
                     vote_average={c.vote_average}
                     />))}
             </div>
-            <CustomPagination setPage={setPage}/>
+            {numOfPages > 1 && (
+            <CustomPagination setPage={setPage} numOfPages={numOfPages} />
+            )} 
         </div>
     );
 }
